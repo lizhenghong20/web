4fcccb5ce8377a83e06995c2dfd908afdd376675
dh <simple.ding816@gmail.com>
2019-04-17 18:22:21 +0800

    添加支付配置

---
+++ China/ravv-service/src/main/java/cn/farwalker/ravv/service/payment/payconfig/biz/impl/PayConfigBizImpl.java
@@ -0,0 +1,18 @@
+package cn.farwalker.ravv.service.payment.payconfig.biz.impl;
+import java.util.List;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import cn.farwalker.ravv.service.payment.payconfig.model.PayConfigBo;
+import cn.farwalker.ravv.service.payment.payconfig.dao.IPayConfigDao;
+import cn.farwalker.ravv.service.payment.payconfig.biz.IPayConfigBiz;
+
+/**
+ * 支付配置<br/>
+ * <br/>
+ * //手写的注释:以"//"开始 <br/>
+ * @author generateModel.java
+ */
+@Service
+public class PayConfigBizImpl extends ServiceImpl<IPayConfigDao,PayConfigBo> implements IPayConfigBiz{
+}
