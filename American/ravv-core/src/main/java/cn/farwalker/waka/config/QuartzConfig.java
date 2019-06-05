ab2c824ab3590594e68a8d8b76b9e9159772c9ae
lee <463428992@qq.com>
2019-06-05 14:17:24 +0800

    更改quartzConfig

--- American/ravv-core/src/main/java/cn/farwalker/waka/config/QuartzConfig.java
+++ American/ravv-core/src/main/java/cn/farwalker/waka/config/QuartzConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
@@ -13,6 +14,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 @Configuration
+@Profile("prod")
 public class QuartzConfig {
 
     @Autowired
